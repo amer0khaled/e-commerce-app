8f7e6aa1abc74af8844f9402c0ce0966b67e6b7b
Amer Khaled <amrkhald144@gmail.com>
2026-01-02 13:37:23 +0200

    feat(payment): publish payment confirmation events to kafka

--- services/payment/src/main/java/com/amer/ecommerce/payment/notification/PaymentNotificationRequest.java
+++ services/payment/src/main/java/com/amer/ecommerce/payment/notification/PaymentNotificationRequest.java
@@ -1,4 +1,30 @@
 package com.amer.ecommerce.payment.notification;
 
-public record PaymentNotificationRequest() {
+import com.amer.ecommerce.payment.api.dto.PaymentRequest;
+import com.amer.ecommerce.payment.domain.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String transactionId,
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+
+) {
+
+    public static PaymentNotificationRequest toNotificationRequest(PaymentRequest request, String transactionId) {
+        return new PaymentNotificationRequest(
+                transactionId,
+                request.orderReference(),
+                request.amount(),
+                request.paymentMethod(),
+                request.customer().firstName(),
+                request.customer().lastName(),
+                request.customer().email()
+        );
+    }
 }
