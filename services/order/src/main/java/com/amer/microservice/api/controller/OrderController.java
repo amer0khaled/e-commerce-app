d4d1d22b6cace79b6f3aceb01a478d1bf96812e7
Amer Khaled <amrkhald144@gmail.com>
2026-01-03 19:08:50 +0200

    (refactor) edit the order controller to depend on abstraction of the order service

--- services/order/src/main/java/com/amer/microservice/api/controller/OrderController.java
+++ services/order/src/main/java/com/amer/microservice/api/controller/OrderController.java
@@ -2,7 +2,7 @@ package com.amer.microservice.api.controller;
 
 import com.amer.microservice.api.dto.OrderRequest;
 import com.amer.microservice.api.dto.OrderResponse;
-import com.amer.microservice.service.impl.OrderServiceImpl;
+import com.amer.microservice.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,25 +15,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderController {
 
-    private final OrderServiceImpl orderServiceImpl;
+    private final OrderService orderService;
 
     @PostMapping
     public ResponseEntity<OrderResponse> createOrder(
             @RequestBody @Valid OrderRequest request
     ) {
-        return ResponseEntity.ok(orderServiceImpl.createOrder(request));
+        return ResponseEntity.ok(orderService.createOrder(request));
     }
 
     @GetMapping
     public ResponseEntity<List<OrderResponse>> findAll() {
-        return ResponseEntity.ok(orderServiceImpl.findAll());
+        return ResponseEntity.ok(orderService.findAll());
     }
 
     @GetMapping("/{order_id}")
     public ResponseEntity<OrderResponse> findById(
             @PathVariable Integer orderId
     ) {
-        return ResponseEntity.ok(orderServiceImpl.findById(orderId));
+        return ResponseEntity.ok(orderService.findById(orderId));
     }
 
 }
